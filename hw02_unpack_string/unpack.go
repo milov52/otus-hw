15964a7c115a4fb711e35a323e377b6e7f6bc6a3
Milov Aleksey <milov52@gmail.com>
2024-06-08 19:34:54 +0300

    Реализация метода распаковки строки

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,58 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isDigit(ch byte) bool {
+	_, err := strconv.Atoi(string(ch))
+	return err == nil
+}
+
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return s, nil
+	}
+	if isDigit(s[0]) {
+		return "", ErrInvalidString
+	}
+
+	var sb strings.Builder
+	escape := false // Флаг для обработки символа '\'
+
+	for i := range s {
+		if escape {
+			sb.WriteByte(s[i])
+			escape = false
+			continue
+		}
+		if s[i] == '\\' {
+			escape = true
+			continue
+		}
+
+		if isDigit(s[i]) {
+			if i+1 < len(s) && isDigit(s[i+1]) {
+				return "", ErrInvalidString
+			}
+
+			n, err := strconv.Atoi(string(s[i]))
+			if err != nil {
+				return "", err
+			}
+			if n == 0 {
+				tmp := sb.String()
+				sb.Reset()
+				sb.WriteString(tmp[:len(tmp)-1])
+			} else {
+				sb.WriteString(strings.Repeat(string(s[i-1]), n-1))
+			}
+		} else {
+			sb.WriteByte(s[i])
+		}
+	}
+	return sb.String(), nil
 }
