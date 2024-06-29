277f311d37537d6e17a919241d131d524120bd87
Milov Aleksey <milov52@gmail.com>
2024-06-29 14:03:14 +0300

    Добавлен код обработчика Run

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,88 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	if len(tasks) == 0 {
+		return nil
+	}
+
+	taskCh := make(chan Task)
+	errCh := make(chan error)
+	errThresholdReached := make(chan struct{})
+	tasksCompleted := make(chan struct{})
+
+	// Горутина для контроля ошибок
+	go func() {
+		defer close(errThresholdReached)
+		errCount := 0
+		for {
+			select {
+			case <-tasksCompleted:
+				return
+			case err := <-errCh:
+				if err != nil {
+					errCount++
+					if errCount >= m {
+						return
+					}
+				}
+			}
+		}
+	}()
+
+	// Запуск воркеров
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-errThresholdReached:
+					return
+				case task, ok := <-taskCh:
+					if !ok {
+						return
+					}
+					if err := task(); err != nil {
+						select {
+						case errCh <- err:
+						case <-errThresholdReached:
+							return
+						}
+					} else {
+					}
+				}
+			}
+		}()
+	}
+
+	// Добавление задач
+	go func() {
+		defer close(taskCh)
+		for _, task := range tasks {
+			select {
+			case <-errThresholdReached:
+				return
+			case taskCh <- task:
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(tasksCompleted)
+
+	select {
+	case <-errThresholdReached:
+		return ErrErrorsLimitExceeded
+	default:
+		return nil
+	}
 }
