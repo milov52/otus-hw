d9db059354d353b3203eeb83c362c1fefb179b44
Milov Aleksey <milov52@gmail.com>
2024-07-01 13:10:45 +0300

    Исправлен код

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,74 +10,56 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// worker запускает задания из taskCh и учитывает ошибки в счетчике errCount.
-// Закрывает канал errThresholdReached, если количество ошибок превышает порог m.
-func worker(
-	taskCh <-chan Task, // Входной канал с задачами
-	wg *sync.WaitGroup, // Группа ожидания для синхронизации горутин
-	errThresholdReached chan struct{}, // Канал для оповещения о превышении порога ошибок
-	once *sync.Once, // Для однократного выполнения закрытия канала
-	m int, // Порог допустимых ошибок
-	errCount *int32, // Счетчик ошибок
-) {
-	defer wg.Done()
-	for {
-		select {
-		case <-errThresholdReached:
-			return
-		case task, ok := <-taskCh:
-			if !ok {
-				return
-			}
-			if err := task(); err != nil {
-				if atomic.AddInt32(errCount, 1) >= int32(m) {
-					once.Do(func() {
-						close(errThresholdReached)
-					})
-					return
-				}
-			}
-		}
-	}
-}
-
-// addTasks отправляет задания в taskCh или прекращает добавление, если errThresholdReached закрыт.
-func addTask(taskCh chan<- Task, tasks []Task, errThresholdReached chan struct{}) {
-	defer close(taskCh)
-	for _, task := range tasks {
-		select {
-		case <-errThresholdReached:
-			return
-		case taskCh <- task:
-		}
-	}
-}
-
 func Run(tasks []Task, n, m int) error {
 	if len(tasks) == 0 {
 		return nil
 	}
 
-	taskCh := make(chan Task)
-	errThresholdReached := make(chan struct{})
-	tasksCompleted := make(chan struct{})
+	taskCh := make(chan Task, len(tasks))
+	stopCh := make(chan struct{})
 	var errCount int32
-	var once sync.Once
 	var wg sync.WaitGroup
+	var once sync.Once
+
+	worker := func() {
+		defer wg.Done()
+		for {
+			select {
+			case task, ok := <-taskCh:
+				if !ok {
+					return
+				}
+				if err := task(); err != nil {
+					if atomic.AddInt32(&errCount, 1) >= int32(m) {
+						once.Do(func() {
+							close(stopCh)
+						})
+						return
+					}
+				}
+			case <-stopCh:
+				return
+			}
+		}
+	}
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go worker(taskCh, &wg, errThresholdReached, &once, m, &errCount)
+		go worker()
 	}
 
-	go addTask(taskCh, tasks, errThresholdReached)
+	for _, task := range tasks {
+		select {
+		case <-stopCh:
+			break
+		case taskCh <- task:
+		}
+	}
+	close(taskCh)
 	wg.Wait()
-	close(tasksCompleted)
 
-	select {
-	case <-errThresholdReached:
+	if errCount >= int32(m) {
 		return ErrErrorsLimitExceeded
-	default:
-		return nil
 	}
+	return nil
 }
