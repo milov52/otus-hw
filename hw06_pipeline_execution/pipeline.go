7ec832939644282f326ed8938388f0ed90bdcd76
Milov Aleksey <milov52@gmail.com>
2024-07-11 09:08:23 +0300

    Добавлена реализация пайплайна

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,51 @@ type (
 
 type Stage func(in In) (out Out)
 
+func executeStage(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		stageOut := stage(in)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-stageOut:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
+}
+
+func workTask(done In, in In, stages ...Stage) Out {
+	var out Out = in
+	for _, stage := range stages {
+		out = executeStage(done, out, stage)
+	}
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := workTask(done, in, stages...)
+
+	result := make(Bi)
+	go func() {
+		defer close(result)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-out:
+				if !ok {
+					return
+				}
+				result <- v
+			}
+		}
+	}()
+	return result
 }
