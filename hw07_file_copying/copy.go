e8afc42eb17f6d9f4d729c5c8c214edfa65105be
Milov Aleksey <milov52@gmail.com>
2024-07-19 20:44:31 +0300

    Реализация комирования файлов

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" || toPath == "" {
+		return ErrUnsupportedFile
+	}
+
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	// Полученим информацию о файле
+	srcFileInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	// Проверка, не превышает ли offset размер файла
+	if offset > srcFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Установка смещения (offset)
+	_, err = srcFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	bar := pb.Full.Start64(srcFileInfo.Size())
+	barReader := bar.NewProxyReader(srcFile)
+
+	if limit > 0 {
+		_, err = io.CopyN(dstFile, barReader, limit)
+	} else {
+		_, err = io.Copy(dstFile, barReader)
+	}
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
 	return nil
 }
