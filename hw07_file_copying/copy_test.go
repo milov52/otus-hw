f3f1f520ae6b97630030019c4962861641e6bc23
Milov Aleksey <milov52@gmail.com>
2024-07-21 11:55:36 +0300

    Исправлены ошибки с линтером

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"io"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCopy(t *testing.T) {
