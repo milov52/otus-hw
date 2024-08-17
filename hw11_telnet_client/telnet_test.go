5e173ae97ab5019e5181afffb8c7f3920a7c778f
Milov Aleksey <milov52@gmail.com>
2024-08-17 10:33:11 +0300

    Добавлены тесты

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -62,4 +62,94 @@ func TestTelnetClient(t *testing.T) {
 
 		wg.Wait()
 	})
+
+	t.Run("connection timeout", func(t *testing.T) {
+		// Тестирование тайм-аута при подключении
+		timeout, err := time.ParseDuration("1ms")
+		require.NoError(t, err)
+
+		client := NewTelnetClient("10.255.255.1:4242", timeout, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
+		err = client.Connect()
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "timeout")
+	})
+
+	t.Run("receive before send", func(t *testing.T) {
+		// Тестирование попытки получения данных до отправки
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			in := &bytes.Buffer{}
+			out := &bytes.Buffer{}
+
+			timeout, err := time.ParseDuration("10s")
+			require.NoError(t, err)
+
+			client := NewTelnetClient(l.Addr().String(), timeout, io.NopCloser(in), out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			err = client.Receive() // Мы ожидаем, что receive будет ожидать данные
+			require.NoError(t, err)
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			defer func() { require.NoError(t, conn.Close()) }()
+
+			_, err = conn.Write([]byte("server data\n"))
+			require.NoError(t, err)
+		}()
+
+		wg.Wait()
+	})
+
+	t.Run("server closes connection", func(t *testing.T) {
+		// Тестирование ситуации, когда сервер закрывает соединение
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			in := &bytes.Buffer{}
+			out := &bytes.Buffer{}
+
+			timeout, err := time.ParseDuration("10s")
+			require.NoError(t, err)
+
+			client := NewTelnetClient(l.Addr().String(), timeout, io.NopCloser(in), out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			err = client.Receive() // Ожидаем завершение при закрытии соединения сервером
+			require.NoError(t, err)
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			require.NoError(t, conn.Close()) // Сервер закрывает соединение сразу после подключения
+		}()
+
+		wg.Wait()
+	})
 }
