71e499f468aa4a65b3b90e15ac0c13b561584b0b
Milov Aleksey <milov52@gmail.com>
2024-08-24 18:30:34 +0300

    Добавлены необходимые структуры данных для хранилищей

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,22 @@
 package storage
 
+import (
+	"time"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID           string
+	Title        string
+	StartTime    time.Time
+	Duration     time.Duration
+	Description  string
+	UserID       string
+	Notification *time.Duration
+}
+
+type Notification struct {
+	EventID string
+	Title   string
+	Date    time.Time
+	UserID  string
 }
