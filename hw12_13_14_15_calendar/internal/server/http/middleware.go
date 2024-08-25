0de1576d7d49bdaae6ddae5650585bccd957d9bc
Milov Aleksey <milov52@gmail.com>
2024-08-25 21:25:25 +0300

    Добавлена реализация middleware

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -1,11 +1,49 @@
 package internalhttp
 
 import (
+	"log/slog"
 	"net/http"
+	"time"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler { //nolint:unused
+func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO
+		startTime := time.Now()
+		// Сохраним оригинальный ResponseWriter для получения статуса ответа
+		ww := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(ww, r)
+
+		latency := time.Since(startTime)
+		clientIP := r.RemoteAddr
+		method := r.Method
+		path := r.URL.Path
+		proto := r.Proto
+		status := ww.statusCode
+		userAgent := r.UserAgent()
+
+		// Пример лога
+		logger := slog.Default() // или использовать s.logger
+		logger.Info("Request",
+			"client_ip", clientIP,
+			"time", startTime.Format(time.RFC1123),
+			"method", method,
+			"path", path,
+			"proto", proto,
+			"status", status,
+			"latency", latency,
+			"user_agent", userAgent,
+		)
 	})
+}
+
+// Обертка для ResponseWriter, чтобы отслеживать статус ответа
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
