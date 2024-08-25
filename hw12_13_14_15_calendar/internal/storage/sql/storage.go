7650c1dde1a73d821512f2863c62e168ea250042
Milov Aleksey <milov52@gmail.com>
2024-08-25 11:18:30 +0300

    Убраны лишние параметры

--- hw12_13_14_15_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_calendar/internal/storage/sql/storage.go
@@ -13,13 +13,11 @@ import (
 )
 
 type Storage struct {
-	dsn  string
 	pool *pgxpool.Pool
 }
 
 func New() *Storage {
 	return &Storage{
-		dsn:  "",
 		pool: nil,
 	}
 }
@@ -50,6 +48,8 @@ func (s *Storage) generateID() string {
 }
 
 func (s *Storage) CreateEvent(ctx context.Context, event storage.Event) (uuid.UUID, error) {
+	const op = "storage.sql.CreateEvent"
+
 	builderInsert := sq.Insert("events").
 		PlaceholderFormat(sq.Dollar).
 		Columns("id", "title", "start_time", "description").
@@ -58,19 +58,21 @@ func (s *Storage) CreateEvent(ctx context.Context, event storage.Event) (uuid.UU
 
 	query, args, err := builderInsert.ToSql()
 	if err != nil {
-		return uuid.UUID{}, fmt.Errorf("failed to build query: %v", err)
+		return uuid.UUID{}, fmt.Errorf("%s: %w", op, err)
 	}
 
 	var eventID uuid.UUID
 	err = s.pool.QueryRow(ctx, query, args...).Scan(&eventID)
 	if err != nil {
-		return uuid.UUID{}, fmt.Errorf("failed to insert event: %v", err)
+		return uuid.UUID{}, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return eventID, nil
 }
 
 func (s *Storage) UpdateEvent(ctx context.Context, id uuid.UUID, event storage.Event) error {
+	const op = "storage.sql.UpdateEvent"
+
 	builderUpdate := sq.Update("events").
 		PlaceholderFormat(sq.Dollar).
 		Set("title", event.Title).
@@ -80,34 +82,38 @@ func (s *Storage) UpdateEvent(ctx context.Context, id uuid.UUID, event storage.E
 
 	query, args, err := builderUpdate.ToSql()
 	if err != nil {
-		return fmt.Errorf("failed to build query: %v", err)
+		return fmt.Errorf("%s: %w", op, err)
 	}
 
 	_, err = s.pool.Exec(ctx, query, args...)
 	if err != nil {
-		return fmt.Errorf("failed to update event: %v", err)
+		return fmt.Errorf("%s: %w", op, err)
 	}
 
 	return nil
 }
 
 func (s *Storage) DeleteEvent(ctx context.Context, id uuid.UUID) error {
+	const op = "storage.sql.DeleteEvent"
+
 	builderDelete := sq.Delete("events").
 		PlaceholderFormat(sq.Dollar).
 		Where(sq.Eq{"id": id})
 
 	query, args, err := builderDelete.ToSql()
 	if err != nil {
-		return fmt.Errorf("failed to build query: %v", err)
+		return fmt.Errorf("%s: %w", op, err)
 	}
 	_, err = s.pool.Exec(ctx, query, args...)
 	if err != nil {
-		return fmt.Errorf("failed to delete event: %v", err)
+		return fmt.Errorf("%s: %w", op, err)
 	}
 	return nil
 }
 
 func (s *Storage) GetEvents(ctx context.Context, date time.Time, offset int) ([]storage.Event, error) {
+	const op = "storage.sql.GetEvents"
+
 	builderSelect := sq.Select("id", "title", "start_time", "description").
 		From("events").
 		PlaceholderFormat(sq.Dollar).
@@ -115,22 +121,22 @@ func (s *Storage) GetEvents(ctx context.Context, date time.Time, offset int) ([]
 
 	query, args, err := builderSelect.ToSql()
 	if err != nil {
-		return nil, fmt.Errorf("failed to build query: %v", err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	rows, err := s.pool.Query(ctx, query, args...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to query events: %v", err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	defer rows.Close()
 	var events []storage.Event
 	for rows.Next() {
 		var event storage.Event
 		if err := rows.Scan(&event.ID, &event.Title, &event.StartTime, &event.Description); err != nil {
-			return nil, fmt.Errorf("failed to scan row: %v", err)
+			return nil, fmt.Errorf("%s: %w", op, err)
 		}
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("failed to iterate rows: %v", err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return events, nil
 }
